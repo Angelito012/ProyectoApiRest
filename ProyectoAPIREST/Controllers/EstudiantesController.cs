5a7a043a404c927fdef783f78873ff5f1d23622e
erickkdaniel <61815795+erickkdaniel@users.noreply.github.com>
2022-09-02 16:05:15 -0600

    API register Estudiante Usuario

---
+++ ProyectoAPIREST/Controllers/EstudiantesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ProyectoAPIREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudiantesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (Models.DataBaseAPIContext db = new Models.DataBaseAPIContext())
+            {
+                var Usuario = (from d in db.Usuarios
+                               select d).ToList();
+                return Ok(Usuario);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Solicitudes.SolicitudEstudiante modelo)
+        {
+            using (Models.DataBaseAPIContext db = new Models.DataBaseAPIContext())
+            {
+                Models.Usuario usuario = new Models.Usuario();
+                usuario.Nombre = modelo.Nombre;
+                usuario.Apellido = modelo.Apellido;
+                usuario.Correo = modelo.Correo;
+                usuario.Telefono = modelo.Telefono;
+                usuario.Nit = modelo.Nit;
+                usuario.TarjetaCredito = modelo.TarjetaCredito;
+                usuario.Contraseña = modelo.Contraseña;
+                usuario.Estado = "A";
+                usuario.Rol = "ESTUDIANTE";
+
+                db.Usuarios.Add(usuario);
+                db.SaveChanges();
+            }
+            return Ok("La empresa se añadio correctamente");
+        }
+    }
+
+
+    
+}
---
+++ ProyectoAPIREST/Models/Solicitudes/SolicitudEstudiante.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoAPIREST.Models.Solicitudes
+{
+    public class SolicitudEstudiante
+    {
+        public int IdUsuario { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Correo { get; set; }
+        public string? Telefono { get; set; }
+        public string? Nit { get; set; }
+        public string? TarjetaCredito { get; set; }
+        public string? Contraseña { get; set; }
+        public string? Estado { get; set; }
+        public string? Rol { get; set; }
+    }
+}
