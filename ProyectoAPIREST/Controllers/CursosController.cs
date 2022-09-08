1a486edb3cab62dc70b961c4a4d0e1bb3a829993
loarca12 <giancrlokin12@gmail.com>
2022-09-08 12:51:08 -0600

    vISTA CURSOS CATALOGO

---
+++ ProyectoAPIREST/Controllers/CursosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAPIREST.Controllers
+{
+    [Route("api/[controller]")]
+  
+    [ApiController]
+    public class CursosController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (Models.DataBaseAPIContext db = new Models.DataBaseAPIContext())
+            {
+                var Curso = (from d in db.Cursos
+                             where d.Estado == "A"
+                              select d).ToList();
+
+                return Ok(Curso);
+            }
+        }
+    }
+}
