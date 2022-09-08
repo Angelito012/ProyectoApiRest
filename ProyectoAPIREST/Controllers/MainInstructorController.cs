633394044ca96016e7a16df1bb0456bcb9e258ab
Monzon99 <monzon.lopez1999@gmail.com>
2022-09-08 11:13:18 -0600

    backend editar instructor

--- ProyectoAPIREST/Controllers/MainInstructorController.cs
+++ ProyectoAPIREST/Controllers/MainInstructorController.cs
@@ -17,7 +17,7 @@ namespace ProyectoAPIREST.Controllers
     {
         [HttpPost]
         [Route("VerCursos")]
-        public ActionResult VerCursos(Models.Solicitudes.AutorizacionUsuarios usuario)
+        public ActionResult VerCursos(Models.Solicitudes.AutorizacionUsuarios instructor)
         {
             List<SolicitudCurso> cursos = new List<SolicitudCurso>();
             using (Models.DataBaseAPIContext db = new Models.DataBaseAPIContext())
@@ -28,7 +28,7 @@ namespace ProyectoAPIREST.Controllers
                 conn.Open();
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.CommandText = "CURSOPROFESOR";
-                cmd.Parameters.Add("@PROFESOR", SqlDbType.VarChar).Value = usuario.correo;
+                cmd.Parameters.Add("@PROFESOR", SqlDbType.VarChar).Value = instructor.correo;
                 SqlDataReader dr = cmd.ExecuteReader();
 
                 while (dr.Read())
@@ -47,6 +47,37 @@ namespace ProyectoAPIREST.Controllers
             }
             
             return Ok(cursos);
+        }
+
+        [HttpPost]
+        [Route("EditarInstructor")]
+        public ActionResult EditarInstructor(SolicitudInstructor instructor)
+        {
+            using(DataBaseAPIContext db = new DataBaseAPIContext())
+            {
+                string conexion = db.connectionString();
+                SqlConnection conn = new SqlConnection(conexion);
+                SqlCommand cmd = conn.CreateCommand();
+                conn.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "EDITARINSTRUCTOR";
+                cmd.Parameters.Add("@NOMBRE", SqlDbType.VarChar).Value = instructor.Nombre;
+                cmd.Parameters.Add("@APELLIDO", SqlDbType.VarChar).Value = instructor.Apellido;
+                cmd.Parameters.Add("@CORREO", SqlDbType.VarChar).Value = instructor.Correo;
+                cmd.Parameters.Add("@CONTRASEÑA", SqlDbType.VarChar).Value = instructor.Contraseña;
+                cmd.Parameters.Add("@TELEFONO", SqlDbType.VarChar).Value = instructor.Telefono;
+                cmd.Parameters.Add("@ESTADO", SqlDbType.VarChar).Value = instructor.Estado;
+                cmd.Parameters.Add("@ROL", SqlDbType.VarChar).Value = instructor.Rol;
+                cmd.Parameters.Add("@EXPERIENCIA", SqlDbType.VarChar).Value = instructor.Experiencia;
+                cmd.Parameters.Add("@CERTIFICACION", SqlDbType.VarChar).Value = instructor.Certificaciones;
+                cmd.Parameters.Add("@BANCO", SqlDbType.VarChar).Value = instructor.NombreBanco;
+                cmd.Parameters.Add("@NOMBRECUENTA", SqlDbType.VarChar).Value = instructor.NombreCuenta;
+                cmd.Parameters.Add("@TIPOCUENTA", SqlDbType.VarChar).Value = instructor.TipoCuenta;
+                cmd.Parameters.Add("@NOCUENTA", SqlDbType.VarChar).Value = instructor.NoCuenta;
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            return Ok();
         }
     }
 }
