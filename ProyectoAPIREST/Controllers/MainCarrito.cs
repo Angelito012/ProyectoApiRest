8f2b6a594a6ba516e50f1035a5a6c9d1b76d2214
azurebot <azure@bot.com>
2022-10-05 12:00:30 -0600

    procedimiento para vaciar carrito

--- ProyectoAPIREST/Controllers/MainCarrito.cs
+++ ProyectoAPIREST/Controllers/MainCarrito.cs
@@ -54,5 +54,24 @@ namespace ProyectoAPIREST.Controllers
             }
             return Ok();
         }
+
+        [HttpDelete]
+        [Route("VaciarCarrito")]
+        public ActionResult VaciarCarrito(SolicitudCarrito carrito)
+        {
+            using (DataBaseAPIContext db = new DataBaseAPIContext())
+            {
+                string conexion = db.connectionString();
+                SqlConnection conn = new SqlConnection(conexion);
+                SqlCommand cmd = conn.CreateCommand();
+                conn.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "VaciarCarrito";
+                cmd.Parameters.Add("@IDUSUARIO", SqlDbType.Int).Value = carrito.IdUsuario;
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            return Ok();
+        }
     }
 }
