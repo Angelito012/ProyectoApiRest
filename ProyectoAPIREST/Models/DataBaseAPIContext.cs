4501c353234d4d8c890922998fc2750fb2619f52
Angelito012 <angelvargasescobar01@gmail.com>
2022-10-10 16:17:42 -0600

    Se agrega el cambio de la base de datos

--- ProyectoAPIREST/Models/DataBaseAPIContext.cs
+++ ProyectoAPIREST/Models/DataBaseAPIContext.cs
@@ -26,7 +26,7 @@ namespace ProyectoAPIREST.Models
 
         public string connectionString()
         {
-            return "Server=DESKTOP-UUF85QJ\\SQLEXPRESS;Database=DataBaseAPI;user=PlusTi;password=12345678";
+            return "Server=DESKTOP-4PEGOM0;Database=DataBaseAPI;user=capacitacion;password=123456789";
 
         }
 
@@ -34,7 +34,7 @@ namespace ProyectoAPIREST.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=DESKTOP-UUF85QJ\\SQLEXPRESS;Database=DataBaseAPI;user=PlusTi;password=12345678");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-4PEGOM0;Database=DataBaseAPI;user=capacitacion;password=123456789");
 
             }
         }
--- ProyectoAPIREST/Views/Instructor/cursos/js/editarCurso.js
+++ ProyectoAPIREST/Views/Instructor/cursos/js/editarCurso.js
@@ -173,7 +173,7 @@ function verLeccion(token){
             contenido_card.innerHTML += `<hr>`
 
             let duracion = document.createElement('h5');
-            duracion.innerText = "Duracion: " + Data[i].duración;
+            duracion.innerText = "Duracion: " + Data[i].duración +" minutos";
             duracion.innerHTML += `<br><br>`;
             contenido_card.appendChild(duracion);
 
