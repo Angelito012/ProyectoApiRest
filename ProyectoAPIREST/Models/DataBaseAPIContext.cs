ffe23f5e669aa76731e3c82b88c31cb26d2ed749
azurebot <azure@bot.com>
2022-10-14 15:47:34 -0600

    Se guardan cambios de segundo reporte

--- ProyectoAPIREST/Models/DataBaseAPIContext.cs
+++ ProyectoAPIREST/Models/DataBaseAPIContext.cs
@@ -26,14 +26,14 @@ namespace ProyectoAPIREST.Models
 
         public string connectionString()
         {
-            return "Server=DESKTOP-57KII1B;Database=DataBaseAPI;user=kevin;password=081999;";
+            return "Server=LAPTOP-0I503ATQ;Database=DataBaseAPI;user=capacitacion;password=loarca12;";
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=DESKTOP-57KII1B;Database=DataBaseAPI;user=kevin;password=081999;");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-0I503ATQ;Database=DataBaseAPI;user=capacitacion;password=loarca12;");
             }
         }
 
